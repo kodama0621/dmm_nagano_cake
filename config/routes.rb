ad865da47f0c32a66c0d117546b0a9fdbbb8b628
EC2 Default User <ec2-user@ip-172-31-58-227.ec2.internal>
2021-04-13 04:32:44 +0000

    [Update]管理者ログインページ作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,11 @@
 Rails.application.routes.draw do
-  devise_for :admin_devises
-  devise_for :admins
+  get 'admins/search/search'
+  get 'admins/homes/top' => 'admins/homes#top'
+  devise_for :admins, controllers: {
+    sessions: 'admin_devises/sessions',
+    passwords: 'admin_devises/passwords',
+    registrations: 'admin_devises/registrations'
+  }
   namespace :admin do
     root to: 'homes#top'
   end
